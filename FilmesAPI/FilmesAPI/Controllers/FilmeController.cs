e88a5650c308403f46df7bfd68cdbcfcbce98fc1
Jorge Mateus <mateusnascimento07@hotmail.com>
2022-06-14 00:07:27 -0300

    Db_context
    Criado dbContext e mapeado a primeira tabela para o banco.

--- FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,30 +12,32 @@ namespace FilmesAPI.Controllers
     [Route("[controller]")]
     public class FilmeController : ControllerBase
     {
-        private static List<Filme> filmes = new List<Filme>();
-        private static int id = 1;
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
         public IActionResult AdicionarFilme([FromBody] Filme filme)
         {
-            filme.Id = id++;
-            filmes.Add(filme);
 
-            Console.WriteLine(filme.Titulo);
-
+            _context.filmes.Add(filme);
+            _context.SaveChanges();
             return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id}, filme);
         }
 
         [HttpGet]
-        public IActionResult RecuperarFilme()
+        public IEnumerable<Filme> RecuperarFilme()
         {
-            return Ok(filmes);
+            return _context.filmes;
         }
 
         [HttpGet("{id}")]
         public IActionResult RecuperaFilmePorId(int id)
         {
-            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            Filme filme = _context.filmes. FirstOrDefault(filme => filme.Id == id);
             if(filme != null )
             {
                 return Ok(filme);
